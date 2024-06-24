7e17b970f9405cd7d5d07de025f5dbaaa3077c8a
Markus Niggemeier <mn@mbss.io>
2024-06-24 15:14:45 +0200

    Add Posts as Default Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
+import Posts from "./Posts";
 
 const Home = () => {
 
 
     return (
-        <div>
-            <h1 className="text-4xl font-bold text-center mt-8">Welcome to the Blog</h1>
-        </div>
+        <Posts/>
     );
 };
 
