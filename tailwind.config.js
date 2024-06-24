b0e31062fa9b2e8af4e9f0ab0fd84e8adfcd5f35
Markus Niggemeier <mn@mbss.io>
2024-06-24 10:24:49 +0200

    Add TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'selector',
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {
+      screens: {
+        'md': '850px',
+      }
+    },
+  },
+  plugins: [
+    require('flowbite/plugin'),
+    require("@catppuccin/tailwindcss")({
+      defaultFlavour: "latte",
+    }),
+  ],
+}
+
