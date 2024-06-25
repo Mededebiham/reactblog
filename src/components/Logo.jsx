4033d09c490481c8d20ff594f46cf6a088102ae9
Markus Niggemeier <mn@mbss.io>
2024-06-25 13:21:53 +0200

    Add Custom Logo

---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../assets/logo.webp';
+
+const defaultClasses = 'scale-125 rounded-full hover:scale-100'
+
+const Logo = ({className}) => {
+    return (
+        <img src={logo} alt="Blog Logo" className={`${defaultClasses} ${className}`}/>
+    );
+};
+
+export default Logo;
