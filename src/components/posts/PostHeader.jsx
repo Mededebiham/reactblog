be147a1b8f91fd5adc2b7dc9c5cab9d54037a844
Markus Niggemeier <mn@mbss.io>
2024-06-25 18:44:41 +0200

    Add Mock Data

--- src/components/posts/PostHeader.jsx
+++ src/components/posts/PostHeader.jsx
@@ -3,6 +3,15 @@ import PostTags from "./PostTags";
 import Link from "../parts/Link";
 
 const PostHeader = ({post}) => {
+
+    const post1 = {
+        author: {
+            name: 'Admin',
+            id: 1,
+        },
+        date: '01.01.1900',
+    }
+
     return (
         <div className="flex justify-between">
             <div>
@@ -12,8 +21,8 @@ const PostHeader = ({post}) => {
             <div>
                 <div
                     className="flex flex-col items-end text-sm justify-start pl-2 border-l-2 border-l-text">
-                    <span className="">01.01.1900</span>
-                    <Link to='#' className="text-blue hover:text-yellow">Admin</Link>
+                    <span className="">{post1.date}</span>
+                    <Link to='#' className="text-blue hover:text-yellow">{post1.author.name}</Link>
                 </div>
             </div>
         </div>
