53cf7d3c1e78caf6dc4f0525f532b3a10dcd5e15
Markus Niggemeier <mn@mbss.io>
2024-06-25 08:24:03 +0200

    Adjust Tag Selector to work with existing Post Tags

--- src/components/TagBadge.jsx
+++ src/components/TagBadge.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const TagBadge = ({ children, onClick, bgColor = "bg-text", key }) => {
-    const className = `${bgColor} text-base text-sm font-medium me-2 px-2.5 py-0.5 rounded-full mb-1`;
+const classNames = "text-base text-sm font-medium me-2 px-2.5 py-0.5 rounded-full mb-1"
+
+const TagBadge = ({ children, onClick, bgColor = "bg-text", key, className = "" }) => {
+    const styles = `${bgColor} ${classNames} ${className}`;
 
     if (onClick) {
         return (
-            <button onClick={onClick} key={key} className={className}>
+            <button onClick={onClick} key={key} className={styles}>
                 {children}
             </button>
         );
     } else {
         return (
-            <span key={key} className={className}>
+            <span key={key} className={styles}>
                 {children}
             </span>
         );
--- src/components/TagSelector.jsx
+++ src/components/TagSelector.jsx
@@ -1,27 +1,63 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import TagBadge from "./TagBadge";
 
 const TagSelector = ({ availableTags, selectedTags, setSelectedTags }) => {
-    const handleTagClick = (tag) => {
-        if (selectedTags.includes(tag)) {
-            setSelectedTags(selectedTags.filter(t => t !== tag));
-        } else {
-            setSelectedTags([...selectedTags, tag]);
+    const tagArray = Object.values(availableTags);
+
+    const [availableTagsForSelection, setAvailableTagsForSelection] = useState([]);
+    const [selectedTagsForSelection, setSelectedTagsForSelection] = useState(selectedTags);
+
+    useEffect(() => {
+        setAvailableTagsForSelection(tagArray.filter(tag => !selectedTagsForSelection.includes(tag)));
+    }, [tagArray, selectedTagsForSelection]);
+
+    const handleTagClickAvailable = (tag) => {
+        if (selectedTagsForSelection.length < 3) {
+            const newSelectedTags = [...selectedTagsForSelection, tag];
+            setSelectedTagsForSelection(newSelectedTags);
+            setSelectedTags(newSelectedTags);
+            const newAvailableTags = availableTagsForSelection.filter((t) => t.id !== tag.id);
+            setAvailableTagsForSelection(newAvailableTags);
         }
+    };
+
+    const handleTagClickSelected = (tag) => {
+        const newSelectedTags = selectedTagsForSelection.filter((t) => t.id !== tag.id);
+        setSelectedTagsForSelection(newSelectedTags);
+        setSelectedTags(newSelectedTags);
+        const newAvailableTags = [...availableTagsForSelection, tag];
+        setAvailableTagsForSelection(newAvailableTags);
     };
 
     return (
         <div className="mt-4">
-            <label className="block text-gray-700 mb-2">Select Tags</label>
-            <div className="flex flex-wrap">
-                {availableTags.map(tag => (
-                    <span
-                        key={tag}
-                        className={`cursor-pointer px-2 py-1 m-1 border rounded ${selectedTags.includes(tag) ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'}`}
-                        onClick={() => handleTagClick(tag)}
-                    >
-                        #{tag}
-                    </span>
-                ))}
+            <div>
+                <label className="block text-text mb-2">Verfügbare Tags</label>
+                <div className="flex flex-wrap">
+                    {availableTagsForSelection.map((tag) => (
+                        <TagBadge
+                            onClick={() => handleTagClickAvailable(tag)}
+                            key={tag.id}
+                            bgColor={tag.color}
+                        >
+                            {tag.name}
+                        </TagBadge>
+                    ))}
+                </div>
+            </div>
+            <div className={`${selectedTagsForSelection.length < 1 ? "hidden" : ""}`}>
+                <label className="block text-text mt-4 mb-2">Ausgewählte Tags</label>
+                <div className="flex flex-wrap">
+                    {selectedTagsForSelection.map((tag) => (
+                        <TagBadge
+                            onClick={() => handleTagClickSelected(tag)}
+                            key={tag.id}
+                            bgColor={tag.color}
+                        >
+                            {tag.name}
+                        </TagBadge>
+                    ))}
+                </div>
             </div>
         </div>
     );
