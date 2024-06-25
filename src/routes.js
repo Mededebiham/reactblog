b8cd052723d148f499041b898326a882964a2a12
Markus Niggemeier <mn@mbss.io>
2024-06-25 07:37:26 +0200

    Localize Nav Links

--- src/routes.js
+++ src/routes.js
@@ -12,13 +12,13 @@ import Settings from "./pages/Settings";
 export const routes = [
 
     { path: '/', component: Home, name: 'Start', renderNav: false, userRole: null },
-    { path: '/posts', component: Posts, name: 'Posts', renderNav: true, userRole: null },
+    { path: '/posts', component: Posts, name: 'Beiträge', renderNav: true, userRole: null },
     { path: '/posts/:id', component: Post, name: 'Post', renderNav: false, userRole: null },
-    { path: '/register',component: Register, name: "Registrieren", renderNav:true, userRole: null },
-    { path: '/login',component: Login, name: "Login", renderNav: false, userRole: null },
-    { path: '/logout',component: Logout, name: "Logout", renderNav: false, userRole: null },
-    { path: '/create-post', component: CreatePost, name: 'Create Post', renderNav: false, userRole: 'admin' },
-    { path: '/settings', component: Settings, name: 'Settings', renderNav: false, userRole: 'user' },
+    { path: '/register',component: Register, name: "Registrieren", renderNav: false, userRole: null },
+    { path: '/login',component: Login, name: "Einloggen", renderNav: false, userRole: null },
+    { path: '/logout',component: Logout, name: "Ausloggen", renderNav: false, userRole: null },
+    { path: '/create-post', component: CreatePost, name: 'Beitrag erstellen', renderNav: true, userRole: 'admin' },
+    { path: '/settings', component: Settings, name: 'Einstellungen', renderNav: false, userRole: 'user' },
     { path: '*', component: Error404, name: '404', renderNav: false, userRole: null }
 
 ];
