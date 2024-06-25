e7e2f476da73c551e2854ec2e33664b807fc3500
mazlum50 <mazlumism50@gmail.com>
2024-06-25 10:02:08 +0200

    Style for Logout Page

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -9,9 +9,11 @@ const Logout = () => {
 
     return (
         <div>
-            <h2>Logout</h2>
-            <p>Möchten Sie sich wirklich abmelden?</p>
-            <button onClick={handleLogout}>Abmelden</button>
+            <div  className="max-w-lg mx-auto p-4 bg-surface0 shadow-md rounded-lg text-text" >
+                <h2 className="text-2xl font-bold mb-4">Logout</h2>
+                <p>Möchten Sie sich wirklich abmelden?</p>
+                <button onClick={handleLogout}  className="w-full p-2 bg-blue text-base rounded hover:bg-sapphire mt-4">Abmelden</button>
+            </div>
         </div>
     );
 };
