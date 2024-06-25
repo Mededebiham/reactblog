a3d060136beb5948429ddfaa2dae39c5acafe5cc
Markus Niggemeier <mn@mbss.io>
2024-06-25 20:55:59 +0200

    Refactor

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {NavLink, useParams} from 'react-router-dom';
+import {useParams} from 'react-router-dom';
 import Link from "../components/parts/Link";
 import {posts} from "../database/mockPostData";
 import TagBadge from "../components/TagBadge";
