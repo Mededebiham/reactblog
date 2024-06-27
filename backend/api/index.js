0883f70f1f47960de522e8e30a10b8cc4f9381c5
Medede Biham <armandbiham@web.de>
2024-06-28 00:22:08 +0200

    Einstellung vercel

---
+++ backend/api/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const connectDB = require('../config/db');
+const postShema=require('../models/User');
+const userShema=require('../models/Post');
+const commentShema=require('../models/Comment');
+const tagsShema=require('../models/Tag');
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// MongoDB Verbindung
+connectDB();
+
+const userRoutes = require('../routes/userRoutes');
+const postRoutes = require('../routes/postRoutes');
+const commentRoutes = require('../routes/commentRoutes');
+const tagRoutes = require('../routes/tagRoutes');
+
+
+// Benutzer, Posts, Kommentare, Tags Routen
+app.use('/api/users', userRoutes);
+app.use('/api/posts', postRoutes);
+app.use('/api/comments', commentRoutes);
+app.use('/api/tags', tagRoutes);
+
+
+// Server starten
+app.listen(PORT, () => {
+    console.log(`Server läuft auf Port ${PORT}`);
+});
+module.exports=app;
