d27b45989e2fff7c2d742467240316b920226001
Markus Niggemeier <mn@mbss.io>
2024-06-28 15:07:49 +0200

    Fix bug where icon was squeezed

--- frontend/src/components/posts/PostHeader.jsx
+++ frontend/src/components/posts/PostHeader.jsx
@@ -3,7 +3,7 @@ import PostTags from "./PostTags";
 import Link from "../parts/Link";
 import { UserContext } from '../../context';
 import { getUserById } from "../../database/db";
-import {toTitleCase} from "../../utils/utils";
+import { toTitleCase } from "../../utils/utils";
 import UserIcon from "../logos/UserIcon";
 
 const PostHeader = ({ post }) => {
@@ -34,16 +34,18 @@ const PostHeader = ({ post }) => {
 
     return (
         <div className="flex justify-between">
-            <div>
+            <div className="flex-grow">
                 <h5 className="mb-2 text-xl font-medium tracking-tight text-text">{post.title}</h5>
                 <PostTags post={post} />
             </div>
-            <div className="flex flex-col items-end ml-4">
-                <div className="flex items-end text-sm justify-start pl-2 border-l-2 border-l-surface2">
-                    <UserIcon className="w-10 h-10 rounded-full mr-2" userId={`${post.userid}`} />
+            <div className="flex flex-col items-end ml-4 flex-shrink-0">
+                <div className="flex items-center text-sm justify-start pl-2 border-l-2 border-l-surface2">
+                    <Link to={`/user/${post.userid}`}>
+                        <UserIcon className="w-10 h-10 rounded-full mr-2" userId={`${post.userid}`} />
+                    </Link>
                     <div className="flex flex-col">
-                    <span className="">{formattedDate}</span>
-                    <Link to={`/user/${post.userid}`} className="text-blue hover:text-yellow">{toTitleCase(author.firstname)}</Link>
+                        <span className="">{formattedDate}</span>
+                        <Link to={`/user/${post.userid}`} className="text-blue hover:text-yellow">{toTitleCase(author.firstname)}</Link>
                     </div>
                 </div>
                 {canEdit && <Link to={`/post/edit/${post._id}`} className="text-sm text-surface0 hover:text-yellow mt-1">Bearbeiten</Link>}
