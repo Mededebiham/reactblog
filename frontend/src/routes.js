cd8146d125dc75e9b86ea78a955b9e1a5892a691
Markus Niggemeier <mn@mbss.io>
2024-06-28 22:02:49 +0200

    Add Routes to Settings

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -7,23 +7,22 @@ import CreatePost from "./pages/CreatePost";
 import Error404 from "./pages/404";
 import Settings from "./pages/Settings";
 import User from "./pages/User";
-
+import { Navigate } from 'react-router-dom';
 
 export const routes = [
-
     { path: '/', component: Home, name: 'Start', renderNav: false, userRole: null },
     { path: '/posts', component: Posts, name: 'Beiträge', renderNav: true, userRole: null },
     { path: '/posts/category/:tag', component: Posts, name: 'Beiträge', renderNav: false, userRole: null },
     { path: '/posts/:page', component: Posts, name: 'Beiträge', renderNav: false, userRole: null},
     { path: '/post/:id', component: Post, name: 'Post', renderNav: false, userRole: null },
     { path: '/post/edit/:id', component: CreatePost, name: 'Beitrag bearbeiten', renderNav: false, userRole: 'admin'},
-    { path: '/register',component: Register, name: "Registrieren", renderNav: false, userRole: null },
-    { path: '/login',component: Login, name: "Einloggen", renderNav: false, userRole: null },
-    { path: 'user/:id', component: User, name: 'Benutzerseite', renderNav: false, userRole: null },
+    { path: '/register', component: Register, name: "Registrieren", renderNav: false, userRole: null },
+    { path: '/login', component: Login, name: "Einloggen", renderNav: false, userRole: null },
+    { path: '/user/:id', component: User, name: 'Benutzerseite', renderNav: false, userRole: null },
     { path: '/create-post', component: CreatePost, name: 'Beitrag erstellen', renderNav: true, userRole: 'admin' },
-    { path: '/settings', component: Settings, name: 'Einstellungen', renderNav: false, userRole: 'user' },
+    { path: '/settings', component: () => <Navigate to="/settings/profile" />, name: 'Einstellungen', renderNav: false, userRole: 'user', exact: true },
+    { path: '/settings/:tab', component: Settings, name: 'Einstellungen', renderNav: false, userRole: 'user' },
     { path: '*', component: Error404, name: '404', renderNav: false, userRole: null }
-
 ];
 
 export default routes;
