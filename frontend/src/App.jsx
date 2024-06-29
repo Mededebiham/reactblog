d2b9508dc613dce2e84229cf98b2b6ab3ff8f986
Markus Niggemeier <mn@mbss.io>
2024-06-29 12:21:17 +0200

    Add Background

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,22 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import ContentRoute from './components/ContentRoute';
 import Footer from './components/Footer';
+import bg from './assets/bg.jpg';
 import FinisherCanvas from "./styles/Particle";
 
 function App() {
     return (
         <Router>
             {/*<FinisherCanvas/>*/}
-            <div className="flex flex-col min-h-screen" style={{ position: 'relative', zIndex: 1 }}>
-                <Navbar />
-                <div className="flex-1 overflow-auto">
-                    <ContentRoute />
+            <div className="flex flex-col min-h-screen relative bg-gradient-to-tr from-subtext1 to-overlay2 dark:from-mantle dark:to-crust">
+                <img src={bg} alt="background-image" className="w-full h-full object-cover absolute mix-blend-overlay" />
+                <div className="relative z-10 flex-1 flex flex-col">
+                    <Navbar />
+                    <div className="flex-1 overflow-auto">
+                        <ContentRoute />
+                    </div>
+                    <Footer />
                 </div>
-                <Footer />
             </div>
         </Router>
     );
