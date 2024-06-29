2053afb80691b4bda526999d3f2ec29aef70e84c
Markus Niggemeier <mn@mbss.io>
2024-06-29 12:42:00 +0200

    Reformat

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import ContentRoute from './components/ContentRoute';
 import Footer from './components/Footer';
 import bg from './assets/bg.jpg';
-import FinisherCanvas from "./styles/Particle";
 
 function App() {
-    return (
-        <Router>
+    return (<Router>
             {/*<FinisherCanvas/>*/}
-            <div className="flex flex-col min-h-screen relative bg-gradient-to-tr from-subtext1 to-overlay2 dark:from-mantle dark:to-crust">
-                <img src={bg} alt="background-image" className="w-full h-full object-cover absolute mix-blend-overlay" />
+            <div
+                className="flex flex-col min-h-screen relative bg-gradient-to-tr from-subtext1 to-overlay2 dark:from-mantle dark:to-crust">
+                <img src={bg} alt="background-image" className="w-full h-full object-cover absolute mix-blend-overlay"/>
                 <div className="relative z-10 flex-1 flex flex-col">
-                    <Navbar />
+                    <Navbar/>
                     <div className="flex-1 overflow-auto">
-                        <ContentRoute />
+                        <ContentRoute/>
                     </div>
-                    <Footer />
+                    <Footer/>
                 </div>
             </div>
-        </Router>
-    );
+        </Router>);
 }
 
 export default App;
