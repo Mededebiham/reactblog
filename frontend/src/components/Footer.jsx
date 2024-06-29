7587ee0cf7ede2582d602f6f4dbda2124a953be1
Markus Niggemeier <mn@mbss.io>
2024-06-29 11:31:31 +0200

    Add Basic Footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
 const Footer = () => {
-    return (
-        <div>
+    const currentYear = new Date().getFullYear();
 
+    return (
+        <div className="bg-mantle text-text flex justify-around">
+            <div className="p-4">© {currentYear} 3 M's</div>
+            <ul className='flex'>
+                <li className="p-4 text-blue hover:text-yellow"><button>Kontakt</button></li>
+                <li className="p-4 text-blue hover:text-yellow"><button>Datenschutz</button></li>
+                <li className="p-4 text-blue hover:text-yellow"><button>Impressum</button></li>
+            </ul>
         </div>
     );
 };
