7973b0c972d3fa40f4961cf7793aa3ecbe39985d
Markus Niggemeier <mn@mbss.io>
2024-07-01 10:51:43 +0200

    Adjust margin for alert

--- frontend/src/components/ContentRoute.jsx
+++ frontend/src/components/ContentRoute.jsx
@@ -4,7 +4,7 @@ import { routes } from "../routes";
 
 const ContentRoute = () => {
     return (
-        <div className="flex justify-center mx-auto max-w-5xl mt-6 h-full overflow-y-auto">
+        <div className="flex justify-center mx-auto max-w-5xl mt-2 h-full overflow-y-auto">
             <Routes>
                 {routes.map((route, index) => (
                     <Route key={index} path={route.path} element={<route.component />} />
